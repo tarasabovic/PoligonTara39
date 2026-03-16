26bf255c5c7473d05cab7c35d5476160a4c13bbd
Ucenik <Ucenik@DESKTOP-VTJ1B2M>
2026-03-16 13:11:52 +0100

    v2

--- Program.cs
+++ Program.cs
@@ -13,11 +13,17 @@ namespace PoligonTara39
             //Tara Sabovic
             // Tacka prva = new Tacka();
             // Console.WriteLine(prva.d());
-            Tacka A = new Tacka(1, 2);
-            Tacka B = new Tacka(2, 3);
-            Vektor AB = new Vektor(A, B);
-            Tacka C = AB.Centriraj();
-            Console.WriteLine(C.d());
+            //Tacka A = new Tacka(1, 2);
+            //Tacka B = new Tacka(2, 3);
+            //Vektor AB = new Vektor(A, B);
+            //Tacka C = AB.Centriraj();
+            //Console.WriteLine(C.d());
+            Tacka A = new Tacka(3,2);
+            Tacka B = new Tacka(-2,3);
+            Tacka O = new Tacka();
+            Vektor OA = new Vektor(O, A);
+            Vektor OB = new Vektor(O, B);
+            Console.WriteLine(Vektor.SP(OA, OB));
         }
     }
 }
